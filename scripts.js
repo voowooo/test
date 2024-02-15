3bd0ede8665b5cf11270fecd5425692085b29cde
jek_a709 <evgenijblagun13@gmail.com>
2024-02-15 15:03:37 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function checkPassword() {
     var password = document.getElementById("passwordBox");
     var passwordText = password.value;
     if(passwordText == "1221") {
-     return true;
+        return true;
     }
     alert("Access denied! Incorrect password!");
     return false;
