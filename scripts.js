e9c115bd8a81096384ebb6f0e2efb77637d30dd0
jek_a709 <evgenijblagun13@gmail.com>
2024-02-15 22:07:50 +0300

    redesigned, login check
    redesigned, login check

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 function checkPassword() {
     var password = document.getElementById("passwordBox");
+    var login = document.getElementById("loginBox");
+    var loginText = login.value;
     var passwordText = password.value;
-    if(passwordText == "1221") {
+    if(passwordText == "1221" & loginText == "jek_a") {
         return true;
     }
-    alert("Access denied! Incorrect password!");
+    alert("Access denied! Incorrect password or login!");
     return false;
     }
